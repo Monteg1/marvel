15e481591e63261359b819524c56afb457c5a9a9
Monteg1 <65545981+Monteg1@users.noreply.github.com>
2024-06-30 15:30:29 +0300

    Add files via upload

---
+++ src/services/MarvelService.js
@@ -0,0 +1,38 @@
+import {useHttp} from '../hooks/http.hook';
+
+const useMarvelService = () => {
+
+  const {loading, request, error, clearError} = useHttp();
+
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+  const _apiKey = 'apikey=77a3f609764b8b0566235f32cf7dbdcf'
+  const _baseOfSet = 210;
+
+  const getAllCharacters = async (ofset = _baseOfSet) => {
+    const res = await request(`${_apiBase}characters?limit=9&offset=${ofset}&${_apiKey}`)
+    return res.data.results.map(_transformCharacter)
+  }
+
+  const getCharacter = async (id) => {
+    const res = await request(`${_apiBase}characters/${id}?${_apiKey}`)
+    return _transformCharacter(res.data.results[0]);
+  }
+
+  const _transformCharacter = (char) => {
+
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items
+    }
+  }
+
+  return {loading, error, getAllCharacters, getCharacter, clearError};
+
+}
+
+export default useMarvelService;
